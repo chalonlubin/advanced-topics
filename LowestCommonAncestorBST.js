1e21211245e4654d2cc0216167307c6a3def7824
Chalon Lubin <chalonlubin1@gmail.com>
2023-02-16 23:43:10 -0800

    started logic for LCA in BST

---
+++ LowestCommonAncestorBST.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+const lowestCommonAncestor = function (root, p, q) {
+  // could use a stack to compare as we travel down from the root,
+  // add each value, including p & q, allow duplicates, and then return the lowest
+  // double value?
+};
