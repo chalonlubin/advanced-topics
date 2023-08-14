7a3d425baff85b4476dd6edb2643227645dd8f1d
Chalon Lubin <chalonlubin1@gmail.com>
2023-08-13 17:00:28 -0700

    Solve groupAnagrams

---
+++ GroupAnagrams.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const groupAnagrams = function(strs) {
+  const result = [];
+  const anagramMap = new Map();
+
+  strs.forEach(str => {
+      const sorted = str.split('').sort().join('')
+
+      if (anagramMap.has(sorted)) {
+          anagramMap.get(sorted).push(str)
+      } else {
+          anagramMap.set(sorted, [str])
+      }
+  })
+
+  anagramMap.forEach((value) => {
+      result.push(value);
+  })
+  return result;
+};
+
+console.log(groupAnagrams(["eat","tea","tan","ate","nat","bat"]));
