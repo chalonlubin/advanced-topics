fbdca3a8d0eb97889ce4b9e3e7ee77b8de75f790
Chalon Lubin <chalonlubin1@gmail.com>
2023-08-13 18:56:11 -0700

    solve ProductArrayExceptSelf

--- ProductArrayExceptSelf.js
+++ ProductArrayExceptSelf.js
@@ -1,15 +1,19 @@
-"use strict";
+'use strict';
 
-const nums = [1,2,3,4];
+const nums = [1, 2, 3, 4];
 // output: [24, 12, 8, 6]
 
+/* This solution times out, when there are many inputs in "nums" due to the
+slice and reduce... */
 function productExceptSelf(nums) {
   const result = [];
 
   for (let i = 0; i < nums.length; i++) {
     const numsCopy = nums.slice();
     numsCopy[i] = 1;
-    const val = numsCopy.reduce((product, number) => product * number)
+    const val = numsCopy.reduce(
+      (product, number) => product * number
+    );
     result.push(val);
   }
 
@@ -17,3 +21,24 @@ function productExceptSelf(nums) {
 }
 
 productExceptSelf(nums);
+
+/* Working solution */
+function productExceptSelf2(nums) {
+  const result = []; //1 , 1 , 2, 6
+
+  let product = 1;
+  for (let i = 0; i < nums.length; i++) {
+    result[i] = product;
+    product *= nums[i];
+  }
+  // console.log(result);
+
+  let suffix = 1; // 1, 4, 12,
+  for (let i = nums.length - 1; i >= 0; i--) {
+    result[i] *= suffix; // [1,1,2,6*1] /  [1,1,8,6] /
+    suffix *= nums[i]; // 1*4=4 / 4*3=12 /
+  }
+
+  return result;
+}
+productExceptSelf2(nums);
